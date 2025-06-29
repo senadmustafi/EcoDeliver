f1865c889ad206120b286bd8db79465014e69fa4
senadmustafi <taddymlg@gmail.com>
2025-06-29 21:03:26 +0200

    Docker update

--- driver-tracking-service/main.py
+++ driver-tracking-service/main.py
@@ -82,4 +82,4 @@ async def update_status(driver_id: str, data: StatusUpdate):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8003, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8003)
